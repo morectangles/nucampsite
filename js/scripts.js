191ac7be306c936435ea2f724ae1fd9ab52f9a5b
Katie Konrad <kathrynkonrad@gmail.com>
2020-02-22 11:40:54 -0500

    Week 4 assignment

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,14 @@ $(function() {
     });
 })
 
+$(function() {
+    $("#reserveButton").click(function() {
+        $("#reserveModal").modal('toggle');
+    });
+})
+
+$(function() {
+    $("#loginButton").click(function() {
+        $("#loginModal").modal('toggle');
+    });
+})
